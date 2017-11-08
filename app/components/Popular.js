a8445c7f72957b1dee113f9286de43fa5769f979
Mike Starr <mstarr@rsui.com>
2017-11-07 21:26:18 -0500

    refacotr popular to stateless component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,32 @@
 //var React = require('react');
 import React from 'react';
+var PropTypes = require('prop-types');
+
+function SelectLanguage(props) {
+    var languages = ['All','JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+    
+    return (       
+
+        <ul className="languages">
+            { languages.map( function (lang) {
+                return (
+                    <li 
+                        style={ lang === props.selectedLanguage ? { color: '#d0021b'}: null } 
+                        onClick={ props.onSelect.bind(null, lang) }
+                        key={lang}>
+                        {lang}
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+
+SelectLanguage.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired
+}
 
 class Popular extends React.Component {
     
@@ -21,22 +48,14 @@ class Popular extends React.Component {
     }
     
     render() {
-        
-        var languages = ['All','JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+                
         return (
-            
-            <ul className="languages">
-                { languages.map( function (lang) {
-                    return (
-                        <li 
-                            style={ lang === this.state.selectedLanguage ? { color: '#d0021b'}: null } 
-                            onClick={ this.updateLanguage.bind(null, lang) }
-                            key={lang}>
-                            {lang}
-                        </li>
-                    )
-                }, this )}
-            </ul>
+            <div>
+                <SelectLanguage 
+                  selectedLanguage={this.state.selectedLanguage}
+                  onSelect={this.updateLanguage}
+                />
+            </div>
         )
     }
 }
