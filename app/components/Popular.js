3e80aa78e50b1bf380ac7334569439ac61811688
Mike Starr <mstarr@rsui.com>
2017-11-07 20:20:40 -0500

    added state to Popular

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,16 +2,40 @@
 import React from 'react';
 
 class Popular extends React.Component {
-    render() {
-        var languages = ['All','JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedLanguage: 'All'
+        }
 
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    updateLanguage(lang) {
+        this.setState(function() {
+            return {
+                selectedLanguage: lang
+            }
+        })
+    }
+    
+    render() {
+        
+        var languages = ['All','JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
         return (
+            
             <ul className="languages">
-                { languages.map( (lang) => 
-                <li key="{lang}">
-                    {lang}
-                </li>
-                )}
+                { languages.map( function (lang) {
+                    return (
+                        <li 
+                            style={ lang === this.state.selectedLanguage ? { color: '#d0021b'}: null } 
+                            onClick={ this.updateLanguage.bind(null, lang) }
+                            key={lang}>
+                            {lang}
+                        </li>
+                    )
+                }, this )}
             </ul>
         )
     }
