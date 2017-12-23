061ed45dc037e30c472aee42b8257eeed046474a
Mike Starr <mstarr@rsui.com>
2017-12-23 14:40:18 -0500

    added results component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,7 @@
 import React from 'react';
 var PropTypes = require('prop-types');
+//var Link = require('react-router-dom').Link;
+import { Link } from 'react-router-dom';
 
 function PlayerPreview(props) {
     return (
@@ -125,7 +127,7 @@ class Battle extends React.Component {
 
 
     render() {
-
+        var match = this.props.match;
         var playerOneName = this.state.playerOneName;
         var playerTwoName = this.state.playerTwoName;
         var playerOneImage = this.state.playerOneImage;
@@ -162,8 +164,21 @@ class Battle extends React.Component {
                             username={playerTwoName}
                             onReset={this.handleChildReset}
                             id='playerTwo'
-                        />}                       
+                        />} 
+
                 </div> 
+
+                {playerOneImage && playerTwoImage && 
+                    <Link
+                        className='button'
+                        to={{
+                            pathname: match.url + '/results',
+                            search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName
+                        }}>
+                        Battle
+                    </Link>
+                }
+                
             </div>
 
         );
